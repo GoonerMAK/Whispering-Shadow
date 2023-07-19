e32572bc38b6a625d3f4c53c85dcc704e823d3d2
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2023-07-19 22:45:20 +0600

    Updated The Pause Menu
    - The Main Menu button in the Pause Menu now works as intended

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -48,7 +48,7 @@ public class PauseMenu : MonoBehaviour
 
     public void MainMenuButton()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(0);
     }
 
 }
